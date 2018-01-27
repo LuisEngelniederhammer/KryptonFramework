f0f514a3e5aefcd27de3fd07da9103dd69952258
LuisEngelniederhammer <lifribjor@live.de>
2018-01-27 21:24:09 +0100

    fixed mission folder structure

---
+++ Krypton/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Krypton;
+
+
+class Database extends \SQLite3
+{
+
+	public function __construct()
+	{
+		$this->open ( realpath ( dirname ( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'database.db', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE, 'seppdepp' );
+		$this->exec ( 'PRAGMA journal_mode = wal;' );
+	}
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+require_once 'Krypton/Page.php';
+
+use Krypton\Page;
+?>
+<?php new Page();?>
+
+
