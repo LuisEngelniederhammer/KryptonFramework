b31c871fa9fdf87cb959692a0070a94df7923047
Luis E <lifribjor@live.de>
2018-01-28 18:43:03 +0100

    Update Database.php

--- Krypton/Database.php
+++ Krypton/Database.php
@@ -8,7 +8,7 @@ class Database extends \SQLite3
 
 	public function __construct()
 	{
-		$this->open ( realpath ( dirname ( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'database.db', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE, 'seppdepp' );
+		$this->open ( realpath ( dirname ( __FILE__ ) ) . DIRECTORY_SEPARATOR . 'database.db', SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE, '' );
 		$this->exec ( 'PRAGMA journal_mode = wal;' );
 	}
 
